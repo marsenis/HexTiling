12d80258bec132f19b1c05520c3c826ac1ccd085
Makis Arsenis <marsenis@gmail.com>
2020-02-20 20:07:51 -0500

    Cleaned up the code and added comments

--- js/hex.js
+++ js/hex.js
@@ -1,16 +1,83 @@
-var canvWidth = 600, canvHeight = 600;
-var hexR = canvWidth / 2;           // Radius of hexagon in pixels
+var W = 600, H = 600;     // Dimensions of canvas
+var R = W / 2;            // Radius of hexagon in pixels
 var hexPhi = Math.PI / 3; // Inner angle of hexagon in radians
+var epsilon = 5;          // Width of source-sink paths
 var n = 4;
-var v, dx, dl, dr;
-var epsilon = 5;
 
-var blocks = [ [4, 3, 3, 0],
-               [3, 2, 1, 0],
-               [2, 1, 1, 0],
-               [1, 1, 1, 0] ];
-var paths;
+// Color palette friendly to certain types of colorblindness
+var yellow = '#FFB000';
+var blue = '#648FFF';
+var red = '#CD267F';
 
+var v;
+var dx, dl, dr;
+
+var blocks, paths;
+
+function main() {
+  var syntaxErrDiv = document.getElementById("errDiv");
+  var alertDiv = document.getElementById("alertDiv");
+
+  syntaxErrDiv.style.display = "none";
+  alertDiv.style.display = "none";
+
+  try {
+    blocks = JSON.parse( document.getElementById("blocksMatrix").value );
+  } catch (err) {
+    syntaxErrDiv.style.display = "block";
+    return;
+  }
+
+  if (!checkValidity(blocks)) {
+    alertDiv.style.display = "block";
+    return;
+  }
+
+  n = blocks.length;
+ 
+  // Append a row of zeros. Needed  in blocksToPaths
+  blocks.push(new Array(n).fill(0));
+
+  // Set canvas size
+  var canvas = document.getElementById('canv');
+  canvas.width = W; canvas.height = H;
+
+  // Blank canvas
+  var path = new Path();
+
+  path.add(new Point(0, 0));
+  path.add(new Point(0, H));
+  path.add(new Point(W, H));
+  path.add(new Point(W, 0));
+  path.add(new Point(0, 0));
+  path.fillColor = 'white';
+
+  // v: verticed of hexagon in clockwise order
+  v = [ new Point(  R*Math.cos(hexPhi), R*Math.sin(hexPhi) ) ];
+  for (var i = 0; i < 5; i++)
+    v.push( rotate(v[i], hexPhi) );
+
+  // Unit vectors in useful directions
+  dx = (v[0] - v[5]) / n;
+  dl = (v[4] - v[5]) / n;
+  dr = (v[1] - v[0]) / n;
+
+  drawHex();
+
+  fillBlanks();
+
+  paths = blocksToPaths(blocks);
+
+  for (var i = 0; i < n; i++)
+    drawSourceSinkPath(i+1, paths[i]);
+}
+
+/*
+ * Input: valid (n+1) x n array of cube stacks
+ *        (n+1)-th row is assumed to be all zeros.
+ * Output: Array of n vertex-disjoint paths
+ *         on the hexagonal grid
+ */
 function blocksToPaths(blocks) {
   var p = new Array(n);
 
@@ -30,6 +97,9 @@ function blocksToPaths(blocks) {
   return p;
 }
 
+/*
+ * Checks if input is a valid representation of stacks
+ */
 function checkValidity(blocks) {
   n = blocks.length;
   for (var i = 0; i < n; i++)
@@ -54,107 +124,39 @@ function checkValidity(blocks) {
   return true;
 }
 
-function init() {
-  var syntaxErrDiv = document.getElementById("errDiv");
-  var alertDiv = document.getElementById("alertDiv");
-
-  syntaxErrDiv.style.display = "none";
-  alertDiv.style.display = "none";
-
-  try {
-    blocks = JSON.parse( document.getElementById("blocksMatrix").value );
-  } catch (err) {
-    syntaxErrDiv.style.display = "block";
-    return;
-  }
-
-  if (!checkValidity(blocks)) {
-    alertDiv.style.display = "block";
-    return;
-  }
-
-  n = blocks.length;
-  blocks.push(new Array(n).fill(0));
-
-  console.log(n);
-
-  // Set canvas size
-  var canvas = document.getElementById('canv');
-  canvas.width = canvWidth; canvas.height = canvHeight;
-
-  // Blank canvas
-  var path = new Path();
-  path.add(new Point(0, 0));
-  path.add(new Point(0, canvHeight));
-  path.add(new Point(canvWidth, canvHeight));
-  path.add(new Point(canvWidth, 0));
-  path.add(new Point(0, 0));
-  path.fillColor = 'white';
-
-  // vs: verticed of hexagon in clockwise order
-  v = [ new Point(  hexR*Math.cos(hexPhi), hexR*Math.sin(hexPhi) ) ];
-  for (var i = 0; i < 5; i++) {
-    v.push( rotate(v[i], hexPhi) );
-  }
-
-  // Unit vectors on useful directions
-  dx = (v[0] - v[5]) / n;
-  dl = (v[4] - v[5]) / n;
-  dr = (v[1] - v[0]) / n;
-
-  drawHex();
-  //drawSourceSinkPath(1, [1, 0, 1, 0, 1, 0]);
-  //drawSourceSinkPath(2, [1, 1, 1, 0, 0, 0]);
-  //drawSourceSinkPath(3, [1, 1, 1, 0, 0, 0]);
-
-  //drawSourceSinkPath(1, [0, 1, 0, 1, 0, 1]);
-  //drawSourceSinkPath(2, [1, 0, 1, 0, 1, 0]);
-  //drawSourceSinkPath(3, [1, 0, 1, 1, 0, 0]);
-
-  /*
-  tilled = new Array(2*n-1);
-  for (var i = 0; i < 2*n; i++)
-    tilled[i] = new Array(n+i+1).fill(false);
-  */
-  
-  fillBlanks();
-
-  paths = blocksToPaths(blocks);
-
-  for (var i = 0; i < n; i++)
-    drawSourceSinkPath(i+1, paths[i]);
-
-  //drawSourceSinkPath(1, [0, 1, 0, 1, 0, 1, 0, 1]);
-  //drawSourceSinkPath(2, [1, 0, 1, 0, 1, 0, 0, 1]);
-  //drawSourceSinkPath(3, [1, 0, 1, 1, 0, 0, 0, 1]);
-  //drawSourceSinkPath(4, [1, 1, 1, 1, 0, 0, 0, 0]);
-
-  console.log(blocksToPaths(blocks));
-}
-
+/*
+ * Initially fills the grid with 'yellow'-type rhombuses
+ */
 function fillBlanks() {
-  console.log("hi");
   for (var r = 0; r < n; r++)
     for (var c = 0; c < n + r + 1; c++) {
       var p = v[5] + dx*c + dl*r;
-      drawRect(p, p + dl, p + dl + dr, p + dr, '#FFB000');
+      drawPoly(p, p + dl, p + dl + dr, p + dr, yellow);
     }
   for (var r = n; r < 2*n - 1; r++)
     for (var c = r - n + 1; c < 2*n; c++) {
       var p = v[5] + dx*c + dl*r;
-      drawRect(p, p + dl, p + dl + dr, p + dr, '#FFB000');
+      drawPoly(p, p + dl, p + dl + dr, p + dr, yellow);
     }
 }
 
+// Rotate vector p by phi radians clockwise
 function rotate(p, phi) {
   return new Point( p.x*Math.cos(phi) + p.y*Math.sin(phi), - p.x*Math.sin(phi) + p.y*Math.cos(phi) );
 }
 
+// Translate coordinate system to match canvas convention
 function fixPoint(p) {
-  return new Point( p.x + canvWidth /2, -p.y + canvHeight / 2 );
+  return new Point( p.x + W /2, -p.y + H / 2 );
 }
 
+/*
+ * Draw a line from p to t of color col and width w
+ * Due to limitations of paper.js, default parameter values
+ * are given in the body of the function
+ */
 function drawLine(p, t, col, width) {
+  // Default value assignment
   if (col == undefined) col = 'black';
   if (width == undefined) width = 1;
 
@@ -167,7 +169,10 @@ function drawLine(p, t, col, width) {
   path.lineTo(t);
 }
 
-function drawRect(a, b, c, d, col) {
+/*
+ * Draw a polygon with vertices a, b, c, d and fill it with col
+ */
+function drawPoly(a, b, c, d, col) {
   drawLine(a, b);
   drawLine(b, c);
   drawLine(c, d);
@@ -191,23 +196,12 @@ function drawSourceSinkPath(source, path) {
   var p = v[5] + dx*(source - 1);
   var r = 0, c = source - 1;
 
-  //drawLine(p, p + dx);
-  //drawLine(p, p + dl);
-  //drawLine(p, p + dr);
-
   for (var i = 0; i < path.length; i++) {
     var t = p + (path[i] ? dr : dl);
-    var col = (path[i] ? '#648FFF' : '#CD267F');
+    var col = (path[i] ? blue : red);
 
-    drawRect(p, t, t + dx, p + dx, col);
+    drawPoly(p, t, t + dx, p + dx, col);
     drawLine(p, t, 'black', epsilon);
-
-    /*
-    if (path[i])
-      tilled[r][++c] = true;
-    else
-      tilled[r][c] = true;
-    */
 
     p = t;
     r++;
@@ -215,13 +209,6 @@ function drawSourceSinkPath(source, path) {
 }
 
 function drawHex() {
-  bgColor = 'orange';
-  fgColor = 'black';
-
-  // Update n value
-  //var k = parseInt(document.getElementById("nValue").value);
-  //if (!isNaN(k)) n = k
-
   // First hexagon vertex
   var p = v[0];
   var dir = (v[5] - p) / n;
@@ -246,6 +233,7 @@ function drawHex() {
   }
 }
 
-globals.init = init
-//window.onload = init;
-init();
+// Make main visible to global scope (outside of paper.js framework)
+// and call it
+globals.main = main;
+main();
